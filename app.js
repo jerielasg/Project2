e49bccf0b4b52d647ebae846b5b5bfdb06dc736f
Haeun <haeun.song@mail.utoronto.ca>
2022-10-20 20:41:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// make a name space
+    // ex. const destinationApp = {};
+
+// get apikey
+    // destinationApp.apikey =
+
+// create init function & call functions below
+
+
+// create a function to get data from the API
+    // ex. destinationApp.getPlaces
+    // get baseUrl and add new search params
+    // call fetch   
+
+// create a function to display data we got from the fetch call 
+    // create div, h2, p, image and append
+
+
+// create a function with addEventlistener
+    // event.preventDefault
+    // user clicks on the dropdown menu, the page reloads with new results
+
+// call init
+// ex. destinationApp.init();
