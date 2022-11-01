1ae57b67b5c1f32371a09b8f73b13452a29dd383
Haeun <haeun.song@mail.utoronto.ca>
2022-11-01 14:26:35 -0400

    added responsive navBar, added fonts

--- app.js
+++ app.js
@@ -22,3 +22,17 @@
 
 // call init
 // ex. destinationApp.init();
+
+document.addEventListener("DOMContentLoaded", () => {
+    // NAV BAR
+    const hamburgerButton = document.querySelector('#hamburger');
+    const closeButton = document.querySelector('#close')
+    const menu = document.querySelector('.slideOutNav');
+
+    hamburgerButton.addEventListener('click', (e) => {
+        menu.classList.add('open');
+    });
+    closeButton.addEventListener('click', (e) => {
+        menu.classList.remove('open');
+    });   
+});
