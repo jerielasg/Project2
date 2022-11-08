a84c44a0b98462311e422540057f5bba1ab11750
Haeun <haeun.song@mail.utoronto.ca>
2022-11-07 19:23:38 -0500

    fixed navbar and modal to load on about page

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+// JS for hamburger, modal
+
+document.addEventListener("DOMContentLoaded", () => {
+    //*********** HAMBURGER MENU *********** //
+    const hamburgerButton = document.querySelector('#hamburger');
+    const closeButton = document.querySelector('#close')
+    const menu = document.querySelector('.slideOutNav');
+
+    hamburgerButton.addEventListener('click', (e) => {
+        menu.classList.add('open');
+    });
+    closeButton.addEventListener('click', (e) => {
+        menu.classList.remove('open');
+    })
+
+    //************** MODAL ****************//
+    const modal = document.querySelector(".modal");
+    const span = document.querySelector(".spanClose");
+    const button = document.querySelector(".modalButton");
+
+    span.addEventListener("click", function () {
+        modal.style.display = "none";
+        modalScore.style.display = "none";
+        modalEndQuestion.style.display = "none"
+    })
+    button.addEventListener("click", function () {
+        modal.style.display = "none";
+    })
+
+    const modalScoreSubmit = document.getElementById('submitQ');
+    const modalScore = document.querySelector(".modalScore");
+    modalScoreSubmit.addEventListener('click', () => {
+        modalScore.style.display = "block";
+        //console.log('testClick1');
+
+    })
+
+    const modalContinue = document.getElementById('modalContinue');
+    const modalEnd = document.querySelector(".modalEndQuestion");
+    modalContinue.addEventListener('click', () => {
+        modalEnd.style.display = "block";
+        modalScore.style.display = "none";
+    })
+
+});
+
 // API call JS 
 
 
@@ -256,49 +302,6 @@ tripApp.init();
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    //*********** HAMBURGER MENU *********** //
-    const hamburgerButton = document.querySelector('#hamburger');
-    const closeButton = document.querySelector('#close')
-    const menu = document.querySelector('.slideOutNav');
-
-    hamburgerButton.addEventListener('click', (e) => {
-        menu.classList.add('open');
-    });
-    closeButton.addEventListener('click', (e) => {
-        menu.classList.remove('open');
-    })
-
-//************** MODAL ****************//
-    const modal = document.querySelector(".modal");
-    const span = document.querySelector(".spanClose");
-    const button = document.querySelector(".modalButton");
-    
-    span.addEventListener("click", function () {
-        modal.style.display = "none";
-        modalScore.style.display = "none";
-        modalEndQuestion.style.display = "none"
-    })
-    button.addEventListener("click", function () {
-        modal.style.display = "none";
-    })
-
-    const modalScoreSubmit = document.getElementById('submitQ');
-    const modalScore = document.querySelector(".modalScore");
-    modalScoreSubmit.addEventListener('click', () => {
-        modalScore.style.display = "block";
-        //console.log('testClick1');
-        
-    })
-
-    const modalContinue = document.getElementById('modalContinue');
-    const modalEnd = document.querySelector(".modalEndQuestion");
-    modalContinue.addEventListener('click', () => {
-        modalEnd.style.display = "block";
-        modalScore.style.display = "none";
-    })
-    
-});
 
 // placeItem.addEventListener("click", function () {
 
